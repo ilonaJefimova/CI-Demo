28c57cf66ec9de033e4eed79b9575bfa4776e406
ilonaJefimova <2101482@uad.ac.uk>
2021-10-26 16:00:21 +0100

    edit code

--- unit-testing-using-dotnet-test/PrimeService/PrimeService.cs
+++ unit-testing-using-dotnet-test/PrimeService/PrimeService.cs
@@ -5,11 +5,9 @@ namespace Prime.Services
     public class PrimeService
     {
         public bool IsPrime(int candidate)
-        {
-            throw new NotImplementedException("Not implemented.");
-        }
-    }
-    public bool IsPrime(int candidate)
-    {if (candidate == 1){return false;}
+        {if (candidate == 1){return false;}
         throw new NotImplementedException("Not fully implemented.");}
+       
+    }
+    
 }
